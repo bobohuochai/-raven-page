eb36f797d050313c9d309ea50a93db8d4d882f24
徐军 <xujun@pingpongx.com>
2022-01-06 13:12:43 +0800

    fix: + update gitignore lib

--- lib/type.d.ts
+++
@@ -1,2 +0,0 @@
-import { Paging, Column, TableConfig, formItemTypeEnum, FormItemConfig, QueryConfig } from './page/pageModel';
-export { Paging, Column, TableConfig, FormItemConfig, formItemTypeEnum, QueryConfig };
